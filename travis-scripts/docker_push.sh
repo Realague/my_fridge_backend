92e9fc359f7aa6616f85198d2e6241ca904ca255
Realague <realague@gmail.com>
2022-06-04 23:37:15 +0200

    [+] config for develop and master

--- travis-scripts/docker_push.sh
+++ travis-scripts/docker_push.sh
@@ -2,28 +2,21 @@
 
 # AWS registry uri
 REGISTRY_URL=public.ecr.aws/p6l5e0y8/my_fridge_backend/services
-SOURCE_IMAGE="${DOCKER_REPO}"
-# using it as there will be 2 versions published
-TARGET_IMAGE="${REGISTRY_URL}"
-#${DOCKER_REPO}
-# lets make sure we always have access to latest image
-TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
-TIMESTAMP=$(date '+%Y%m%d%H%M%S')
-# using datetime as part of a version for versioned image
-VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
-# using specific version as well
-# it is useful if you want to reference this particular version
-# in additional commands like deployment of new Elasticbeanstalk version
-TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
+TARGET_IMAGE_LATEST="${REGISTRY_URL}:latest"
+TARGET_IMAGE_VERSIONED="${REGISTRY_URL}:${TRAVIS_TAG}"
 
 # Push image to ECR
 ###################
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/p6l5e0y8
 
-# update latest version
-docker tag ${SOURCE_IMAGE} "${REGISTRY_URL}:latest"
-docker push "${REGISTRY_URL}:latest"
-
-# push new version
-#docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_VERSIONED}
-#docker push ${TARGET_IMAGE_VERSIONED}
+if [ ! -z "${TRAVIS_TAG}" ]
+then
+  # push new version
+  docker tag ${IMAGE_NAME} ${TARGET_IMAGE_VERSIONED}
+  docker push ${TARGET_IMAGE_VERSIONED}
+fi
+else
+  # update latest version
+  docker tag ${IMAGE_NAME} "${REGISTRY_URL}:latest"
+  docker push "${REGISTRY_URL}:latest"
+fi
